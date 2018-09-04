1b45f50fbd84a748aa637bd91cd14f2ec233016d
Jacob Casado <jacob.casado.perez@gmail.com>
2018-09-04 11:16:07 +0200

    Solucionado el problema con los audios

--- js/quiz.js
+++ js/quiz.js
@@ -10,39 +10,33 @@ var pos = 0,
   chD,
   chE,
   correct = 0;
-// function playAudio() {
-//   var x = document.getElementById("myAudio");
-//   x.play();
-// }
-// function pauseAudio() {
-//   var x = document.getElementById("myAudio");
-//   x.pause();
-// }
+
 var isPlaying = false;
-function togglePlay() {
-  var myAudio = document.getElementById("myAudio");
+function togglePlay(event) {
+  var myAudio = document.getElementById(event.target.id + "Audio");
+  console.log(myAudio);
 
   if (isPlaying) {
     myAudio.pause();
     isPlaying = false;
-    document.getElementById("playAudio").innerHTML = "PAUSE";
+    document.getElementById(event.target.id).innerHTML = "PLAY";
   } else {
     myAudio.play();
     isPlaying = true;
-    document.getElementById("playAudio").innerHTML = "PLAY";
+    document.getElementById(event.target.id).innerHTML = "PAUSE";
   }
 }
 
 var questions = [
   [
-    '¿Quién compuso esta BSO? <audio id= "myAudio" src="./MoviesClips/Batman89-theme.flac"></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
+    '¿Quién compuso esta BSO? <audio id= "playAudioAudio" src="./MoviesClips/Batman89-theme.flac"></audio><button id= "playAudio" onclick="togglePlay(event)">PLAY</button>',
     "Hans Zimmer",
     "John Williams",
     "Danny Elfman",
     "C"
   ],
   [
-    '¿A qué película pertenece esta BSO? <audio id= "myAudio" src="./MoviesClips/BackToTheFuture-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
+    '¿A qué película pertenece esta BSO? <audio id= "playAudioAudio" src="./MoviesClips/BackToTheFuture-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay(event)">PLAY</button>',
     "Indiana Jones",
     "Back To The Future",
     "Jurassic Park",
@@ -50,20 +44,20 @@ var questions = [
   ],
   [
     "¿Qué BSO no pertenece a Basil Poledouris?",
-    '<audio  id= "myAudio" src="./MoviesClips/TotalRecall-theme.flac" controls></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
-    '<audio id= "myAudio" src="./MoviesClips/Conan-theme.flac" controls></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
-    '<audio id= "myAudio" src="./MoviesClips/Robocop-theme.flac"controls></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
+    '<audio id= "myAudio1Audio" src="./MoviesClips/TotalRecall-theme.flac" controls></audio></audio><button id= "myAudio1" onclick="togglePlay(event)">PLAY</button>',
+    '<audio id= "myAudio2Audio" src="./MoviesClips/Conan-theme.flac" controls></audio></audio><button id= "myAudio2" onclick="togglePlay(event)">PLAY</button>',
+    '<audio id= "myAudio3Audio" src="./MoviesClips/Robocop-theme.flac"controls></audio></audio><button id= "myAudio3" onclick="togglePlay(event)">PLAY</button>',
     "A"
   ],
   [
-    '¿De qué película es esta BSO? <audio id= "myAudio" hidden src="./MoviesClips/RequiemForADream-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
+    '¿De qué película es esta BSO? <audio id= "playAudioAudio" src="./MoviesClips/RequiemForADream-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay(event)">PLAY</button>',
     "The Fountain",
     "Memento",
     "Requiem for a Dream",
     "C"
   ],
   [
-    '¿A quién pertenece esta BSO de Superman? <audio id= "myAudio" hidden src="./MoviesClips/Superman10-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
+    '¿A quién pertenece esta BSO de Superman? <audio id= "playAudioAudio" src="./MoviesClips/Superman10-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay(event)">PLAY</button>',
     "Hans Zimmer",
     "John Williams",
     "Alan Silvestri",
@@ -71,20 +65,20 @@ var questions = [
   ],
   [
     "¿Qué BSO no pertenece a Hans Zimmer?",
-    '<audio id= "myAudio" hidden src="./MoviesClips/PiratesOfCaribbean-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
-    '<audio id= "myAudio" hidden src="./MoviesClips/TheRock-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
-    '<audio id= "myAudio" hidden src="./MoviesClips/Terminator-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
+    '<audio id= "myAudio4Audio" src="./MoviesClips/PiratesOfCaribbean-theme.flac"></audio></audio><button id= "myAudio4" onclick="togglePlay(event)">PLAY</button>',
+    '<audio id= "myAudio5Audio" src="./MoviesClips/TheRock-theme.flac"></audio></audio><button id= "myAudio5" onclick="togglePlay(event)">PLAY</button>',
+    '<audio id= "myAudio6Audio" src="./MoviesClips/Terminator-theme.flac"></audio></audio><button id= "myAudio6" onclick="togglePlay(event)">PLAY</button>',
     "C"
   ],
   [
-    '¿Quién es el compositor de esta BSO? <audio id= "myAudio" hidden src="./MoviesClips/ThePinkPanther-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
+    '¿Quién es el compositor de esta BSO? <audio id= "playAudioAudio" src="./MoviesClips/ThePinkPanther-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay(event)">PLAY</button>',
     "Henry Mancini",
     "Nino Rota",
     "Ennio Morricone",
     "A"
   ],
   [
-    '¿A qué película pertenece esta BSO? <audio id= "myAudio" hidden src="./MoviesClips/KillBill-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
+    '¿A qué película pertenece esta BSO? <audio id= "playAudioAudio" src="./MoviesClips/KillBill-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay(event)">PLAY</button>',
     "Reservoir Dogs",
     "Pull Fiction",
     "Kill Bill",
@@ -92,16 +86,16 @@ var questions = [
   ],
   [
     "¿Qué BSO no pertenece a Ennio Morricone?",
-    '<audio id= "myAudio" hidden src="./MoviesClips/TheGodfather-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
-    '<audio id= "myAudio" hidden src="./MoviesClips/TheMission-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
-    '<audio id= "myAudio" hidden src="./MoviesClips/TheUntouchables-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
+    '<audio id= "myAudio7Audio" src="./MoviesClips/TheGodfather-theme.flac"></audio></audio><button id= "myAudio7" onclick="togglePlay(event)">PLAY</button>',
+    '<audio id= "myAudio8Audio" src="./MoviesClips/TheMission-theme.flac"></audio></audio><button id= "myAudio8" onclick="togglePlay(event)">PLAY</button>',
+    '<audio id= "myAudio9Audio" src="./MoviesClips/TheUntouchables-theme.flac"></audio></audio><button id= "myAudio9" onclick="togglePlay(event)">PLAY</button>',
     "A"
   ],
   [
     "¿Qué BSO pertenece a Star Wars?",
-    '<audio id= "myAudio" hidden src="./MoviesClips/ET-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
-    '<audio id= "myAudio" hidden src="./MoviesClips/Superman70-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
-    '<audio id= "myAudio" hidden src="./MoviesClips/StarWars-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
+    '<audio id= "myAudio10Audio" src="./MoviesClips/ET-theme.flac"></audio></audio><button id= "myAudio10" onclick="togglePlay(event)">PLAY</button>',
+    '<audio id= "myAudio11Audio" src="./MoviesClips/Superman70-theme.flac"></audio></audio><button id= "myAudio11" onclick="togglePlay(event)">PLAY</button>',
+    '<audio id= "myAudio12Audio" src="./MoviesClips/StarWars-theme.flac"></audio></audio><button id= "myAudio12" onclick="togglePlay(event)">PLAY</button>',
     "C"
   ]
 ];
@@ -109,11 +103,11 @@ var questions = [
 var finalQuestion = [
   [
     "Ronda Final: Ordena esta BSO arrastrando en su casilla correcta",
-    '<audio id= "myAudio" hidden src="./MoviesClips/ImperialMarch3-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
-    '<audio id= "myAudio" hidden src="./MoviesClips/ImperialMarch5-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
-    '<audio id= "myAudio" hidden src="./MoviesClips/ImperialMarch4-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
-    '<audio id= "myAudio" hidden src="./MoviesClips/ImperialMArch1-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
-    '<audio id= "myAudio" hidden src="./MoviesClips/ImperialMarch2-theme.flac"></audio></audio><button id= "playAudio" onclick="togglePlay()">PLAY</button>',
+    '<audio id= "myAudio13Audio" src="./MoviesClips/ImperialMarch3-theme.flac"></audio></audio><button id= "myAudio13" onclick="togglePlay(event)">PLAY</button>',
+    '<audio id= "myAudio14Audio" src="./MoviesClips/ImperialMarch5-theme.flac"></audio></audio><button id= "myAudio14" onclick="togglePlay(event)">PLAY</button>',
+    '<audio id= "myAudio15Audio" src="./MoviesClips/ImperialMarch4-theme.flac"></audio></audio><button id= "myAudio15" onclick="togglePlay(event)">PLAY</button>',
+    '<audio id= "myAudio16Audio" src="./MoviesClips/ImperialMArch1-theme.flac"></audio></audio><button id= "myAudio16" onclick="togglePlay(event)">PLAY</button>',
+    '<audio id= "myAudio17Audio" src="./MoviesClips/ImperialMarch2-theme.flac"></audio></audio><button id= "myAudio17" onclick="togglePlay(event)">PLAY</button>',
     "A",
     "B",
     "C",
