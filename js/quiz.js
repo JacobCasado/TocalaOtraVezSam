892fe59061a58870817c0903f61d0385fb7cb7fe
Jacob Casado <jacob.casado.perez@gmail.com>
2018-09-07 09:54:54 +0200

    Añadida musica a la intro

--- js/quiz.js
+++ js/quiz.js
@@ -1,6 +1,10 @@
 window.onload = function() {
+  var music = new Audio("./MoviesClips/LOTR-theme.flac")
+  music.loop = true;
+  music.play();
   document.getElementById("start-button").onclick = function() {
     
+    music.pause();
     $('.game-intro').css('display','none');
     $('.game').css('display','block');
     chronometre.set();
